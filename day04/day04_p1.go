b00ccbee154bb905bc1daa175bc7a266410477b6
knlnks <soulkks@hotmail.ca>
2023-12-04 00:59:10 -0800

    ⭐ Part 1

---
+++ day04/day04_p1.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	// Open the input file
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	// Create a buffered reader
+	reader := bufio.NewReader(file)
+
+	// Total of all cards
+	total := 0
+
+	// iterate over each card
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+
+		// Skip the `Card   1: ` part
+		var i int
+		for i = 3; line[i-2] != ':'; i++ {
+		}
+
+		tempNum := 0
+
+		// Read the winning numbers
+		var winningNums [10]int
+		for c := 0; line[i] != '|'; i++ {
+			switch line[i] {
+			case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+				tempNum = (tempNum * 10) + int(line[i]-'0')
+			default:
+				if tempNum != 0 {
+					winningNums[c] = tempNum
+					tempNum = 0
+					c++
+				}
+			}
+		}
+
+		// Skip to first number
+		i += 2
+
+		// Card point total
+		cardTotal := 0
+
+		// Read the card numbers
+		for ; i < len(line); i++ {
+			switch line[i] {
+			case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+				tempNum = (tempNum * 10) + int(line[i]-'0')
+			default:
+				if tempNum != 0 {
+					for j := range winningNums {
+						if tempNum == winningNums[j] {
+							if cardTotal == 0 {
+								cardTotal++
+							} else {
+								cardTotal *= 2
+							}
+							break
+						}
+					}
+					tempNum = 0
+				}
+			}
+		}
+
+		total += cardTotal
+	}
+
+	// Print the total
+	fmt.Println(total)
+}
